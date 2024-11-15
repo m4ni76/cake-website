e0a12d448b2b9ee260e36790673351582a6c5f85
m4ni76 <m4ni4321@gmail.com>
2024-11-15 22:54:52 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <ContactForm />
+    </div>
+  );
+}
+export default Contact;
