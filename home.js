283a1b3a68ab9c3b17e518a1fc91c348143b5ddc
m4ni76 <m4ni4321@gmail.com>
2024-11-15 22:58:33 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Cake Heaven!</h2>
+      <p>Your destination for the most delicious cakes.</p>
+    </div>
+  );
+}
+export default Home;
