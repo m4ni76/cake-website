bf5529b7a6de04b1bebf501ea07daf47f6521f7b
m4ni76 <m4ni4321@gmail.com>
2024-11-15 22:49:07 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>We are passionate about baking the best cakes in town!</p>
+    </div>
+  );
+}
+
+export default About;
