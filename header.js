507afda3c1bdcad65784666927160e0ee71c6409
m4ni76 <m4ni4321@gmail.com>
2024-11-15 22:57:45 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Header() {
+  return (
+    <header>
+      <h1>Cake Heaven</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cakes">Cakes</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
+export default Header;
